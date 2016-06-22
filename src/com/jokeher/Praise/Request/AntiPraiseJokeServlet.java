8b0915d943a53cb378bb88685f7c283bdff247ff
Silen <jianmofly@gmail.com>
2016-06-22 21:53:04 +0800

    完成点赞系统

--- src/com/jokeher/Praise/Request/AntiPraiseJokeServlet.java
+++ src/com/jokeher/Praise/Request/AntiPraiseJokeServlet.java
@@ -10,30 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 /**
- * Created by 10397 on 2016/6/21.
- * 点赞API
+ * Created by 10397 on 2016/6/22.
+ * 踩API
  * <pre>
  *     URL:http://localhost:8080/Praise/PraiseJoke?userId=&jokeId=
  *     userId:user的id,int
  *     jokeId:joke的id,int
- *     当点赞存在时，删除记录
- *     当为踩时，点赞，删除踩
- *     当点赞不存在时，新增
+ *     当踩存在时，删除记录
+ *     当为赞时，踩，删除赞
+ *     当踩不存在时，新增
  *     return:JSON
- *     {"success":true,"funny":true,"boring":false,"jokeId":1}
+ *     {"success":true,"funny":false,"boring":true,"jokeId":1}
  *     操作结果，success代表是否有错误，funny：赞，boring：踩，jokeId：joke的id
  * </pre>
  */
-@WebServlet("/Praise/PraiseJoke")
-public class PraiseJokeServlet extends HttpServlet {
+@WebServlet("/Praise/AntiPraiseJoke")
+public class AntiPraiseJokeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             int userId = Integer.parseInt(req.getParameterValues("userId")[0]);
             int jokeId = Integer.parseInt(req.getParameterValues("jokeId")[0]);
-            resp.getWriter().write(new Gson().toJson(PraiseService.praiseJoke(userId, jokeId)));
+            resp.getWriter().write(new Gson().toJson(PraiseService.antiPraiseJoke(userId, jokeId)));
         }catch (Exception e){
             e.printStackTrace();
         }
--- src/com/jokeher/Praise/Service/PraiseService.java
+++ src/com/jokeher/Praise/Service/PraiseService.java
@@ -1,6 +1,7 @@
 package com.jokeher.Praise.Service;
 
 import com.jokeher.MySQL.SQLBase;
+import com.jokeher.Praise.Model.PraiseReturn;
 
 import java.util.List;
 import java.util.Map;
@@ -9,12 +10,76 @@ import java.util.Map;
  * Created by 10397 on 2016/6/21.
  */
 public class PraiseService {
-    private static final String QUARY_PARISE_EXIST_SQL = "SELECT * FROM praise_joke WHERE userId=%d and jokeId=%d";
 
-    public static void praiseJoke(int userId,int jokeId){
-        SQLBase sqlBase=new SQLBase();
-        List<Map<String,Object>> list=sqlBase.queryDateWithReturn(String.format(QUARY_PARISE_EXIST_SQL,new Object[]{userId,jokeId}));
+    private static final String QUERY_PRAISE_EXIST_SQL = "SELECT * FROM praise_joke WHERE userId=%d and jokeId=%d";
+    private static final String UPDATE_JOKE_SQL = "UPDATE joke SET isFunny=isFunny+%d,isBoring=isBoring+%d,time=%d WHERE jokeId=%d";
+    private static final String UPDATE_JOKE_PRAISE_SQL = "UPDATE praise_joke SET isFunny=%b,time=%d WHERE userId=%d and jokeId=%d";
+    private static final String DELETE_JOKE_PRAISE_SQL = "DELETE FROM praise_joke WHERE userId=%d and jokeId=%d";
+    private static final String INSERT_JOKE_PRAISE_SQL = "INSERT INTO praise_joke(userId,jokeId,isFunny,time) VALUES(%d,%d,%b,%d)";
 
+    public static PraiseReturn praiseJoke(int userId, int jokeId) {
+        PraiseReturn praiseReturn = new PraiseReturn();
+        praiseReturn.setJokeId(jokeId);
+        try {
+            SQLBase sqlBase = new SQLBase();
+            List<Map<String, Object>> list = sqlBase.queryDateWithReturn(String.format(QUERY_PRAISE_EXIST_SQL, new Object[]{userId, jokeId}));
+            if (null != list && list.size() > 0) {
+                if ((boolean) list.get(0).get("isFunny")) {
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{-1, 0, System.currentTimeMillis(), jokeId}));
+                    sqlBase.executeSQL(String.format(DELETE_JOKE_PRAISE_SQL, new Object[]{userId, jokeId}));
+                    praiseReturn.setFunny(false);
+                    praiseReturn.setBoring(false);
+                } else {
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{1, -1, System.currentTimeMillis(), jokeId}));
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_PRAISE_SQL, new Object[]{true, System.currentTimeMillis(), userId, jokeId}));
+                    praiseReturn.setFunny(true);
+                    praiseReturn.setBoring(false);
+                }
+            } else {
+                sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{1, 0, System.currentTimeMillis(), jokeId}));
+                sqlBase.executeSQL(String.format(INSERT_JOKE_PRAISE_SQL, new Object[]{userId, jokeId, true, System.currentTimeMillis()}));
+                praiseReturn.setFunny(true);
+                praiseReturn.setBoring(false);
+            }
+            praiseReturn.setSuccess(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return praiseReturn;
+    }
 
+    public static PraiseReturn antiPraiseJoke(int userId, int jokeId) {
+
+        PraiseReturn praiseReturn = new PraiseReturn();
+        praiseReturn.setJokeId(jokeId);
+
+        try {
+            SQLBase sqlBase = new SQLBase();
+            List<Map<String, Object>> list = sqlBase.queryDateWithReturn(String.format(QUERY_PRAISE_EXIST_SQL, new Object[]{userId, jokeId}));
+            if (null != list && list.size() > 0) {
+                if (!(boolean) list.get(0).get("isFunny")) {
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{0, -1, System.currentTimeMillis(), jokeId}));
+                    sqlBase.executeSQL(String.format(DELETE_JOKE_PRAISE_SQL, new Object[]{userId, jokeId}));
+                    praiseReturn.setFunny(false);
+                    praiseReturn.setBoring(false);
+                } else {
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{-1, 1, System.currentTimeMillis(), jokeId}));
+                    sqlBase.executeSQL(String.format(UPDATE_JOKE_PRAISE_SQL, new Object[]{false, System.currentTimeMillis(), userId, jokeId}));
+                    praiseReturn.setFunny(false);
+                    praiseReturn.setBoring(true);
+                }
+            } else {
+                sqlBase.executeSQL(String.format(UPDATE_JOKE_SQL, new Object[]{0, 1, System.currentTimeMillis(), jokeId}));
+                sqlBase.executeSQL(String.format(INSERT_JOKE_PRAISE_SQL, new Object[]{userId, jokeId, false, System.currentTimeMillis()}));
+                praiseReturn.setFunny(false);
+                praiseReturn.setBoring(true);
+            }
+            praiseReturn.setSuccess(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            praiseReturn.setSuccess(false);
+        }
+        return praiseReturn;
     }
 }
+
